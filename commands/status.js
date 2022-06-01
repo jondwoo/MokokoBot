404ed0cebb2e07e36c279267b7a8344d5c0f2281
jon ham <jondwoo@gmail.com>
2022-06-01 13:40:58 -0700

    server status

--- commands/status.js
+++ commands/status.js
@@ -1,16 +1,16 @@
 import { SlashCommandBuilder } from '@discordjs/builders'
+import getJSONResponse from '../util/getJSONResponse.js'
+import { request } from 'undici'
 
 export default {
   data: new SlashCommandBuilder()
     .setName('status')
     .setDescription('Replies with status of all servers'),
   async execute(interaction) {
-    const res = await axios
-      .get("http://lostarkapi.herokuapp.com/server/all")
-      .catch(function(response) {
-        console.log(response);
-      });
+    const rawData = await request("http://lostarkapi.herokuapp.com/server/all")
+    const res = await getJSONResponse(rawData.body);
+    console.log(res.data)
 
-    await interaction.reply(res.data.data);
+    return interaction.reply(`Rohendel: ${res.data.Rohendel}`)
   },
 };
